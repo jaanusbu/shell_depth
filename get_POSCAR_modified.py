557295eb82d7a2c5bad16398354260e36fcdf9f9
jaanusbu <jaanus11@hotmail.com>
2021-01-08 11:16:25 +0200

    updating files

--- get_POSCAR_modified.py
+++ get_POSCAR_modified.py
@@ -1,4 +1,7 @@
-def xyz2POSCAR(xyz_file_name, out_file_name):
+from math import sqrt
+import numpy as np
+
+def xyz2POSCAR(xyz_file_name, out_file_name='POSCAR',move_COM=True):
     xyz_file = xyz_file_name
     out_file = out_file_name
     def get_box(xyz_file):
@@ -51,8 +54,17 @@ def xyz2POSCAR(xyz_file_name, out_file_name):
 
     poscar = open(out_file, 'w')
     poscar.write('Converted_POSCAR\n1.0\n')
+    i_x = 0 # Needed to move the NP if COM is 0,0,0
+    i_y = 0
+    i_z = 0
     for i in get_box(xyz_file):
         poscar.write('%s %s %s\n' %(i[0], i[1], i[2]))
+        if float(i[0]) != 0:
+            i_x = float(i[0])
+        if float(i[1]) != 0:
+            i_y = float(i[1])
+        if float(i[2]) != 0:
+            i_z = float(i[2])
 
 # Write Elements line 
     for i in get_total_ele(xyz_file):
@@ -66,9 +78,32 @@ def xyz2POSCAR(xyz_file_name, out_file_name):
 # Write Cartesian line 
     poscar.write('\nCartesian\n')
 
-# Write Cooridination part 
+# Finding the Center-Of-Mass
+    XYZ = []
     for i in get_total_ele(xyz_file):
         for j in get_coordinations(i):
-           poscar.write('%s %s %s %s %s %s\n' %(j[1], j[2], j[3], j[5], j[6], j[7]))
+            XYZ.append([float(j[1]),float(j[2]),float(j[3])])
+    points = np.array(XYZ)
+    test = [[1] for i in range(len(points))]
+    c = np.append(points,test,axis=1)
+    COM = np.average(c[:,:3], axis=0, weights= c[:,3])
+    mover = []
+    
+# Finding need for moving the Center-Of-Mass
+    if sqrt(COM[0]**2+COM[1]**2+COM[2]**2) != sqrt((i_x/2)**2+(i_y/2)**2+(i_z/2)**2):
+        mover.append((i_x/2) - COM[0])
+        mover.append((i_y/2) - COM[1])
+        mover.append((i_z/2) - COM[2])
+        
+# Write Cooridination part 
+    for i in get_total_ele(xyz_file):
+        if len(mover) != 0 and move_COM:
+#            print("ver1")
+            for j in get_coordinations(i):
+                poscar.write('%s %s %s %s %s %s\n' %(float(j[1])-mover[0], float(j[2])-mover[1], float(j[3])-mover[2], j[5], j[6], j[7]))
+        else:
+#            print("ver2")
+            for j in get_coordinations(i):
+                poscar.write('%s %s %s %s %s %s\n' %(j[1], j[2], j[3], j[5], j[6], j[7]))
 
     print(('Done! the POSCAR is named as %s') %(out_file))
